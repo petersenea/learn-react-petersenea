513d176851b0fdca02abf9132463cf93fd2c7d6d
petersenea <Petersenea12@gmail.com>
2020-04-15 22:07:50 -0500

    styled static catalog page with images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { Card, Column, Button } from 'rbx';
+import "rbx/index.css";
+
 
 const App = () => {
   const [data, setData] = useState({});
@@ -13,9 +16,53 @@ const App = () => {
   }, []);
 
   return (
-    <ul>
-      {products.map(product => <li key={product.sku}>{product.title}</li>)}
-    </ul>
+    <Column.Group multiline={true}>
+      {products.map(product =>
+        <Column size="one-quarter">
+          <Card>
+            {/* <Card.Header>
+              {product.title}
+            </Card.Header> */}
+            <Card.Image>
+              <img src={"data/products/" + product.sku + "_2.jpg"} alt="product"></img>
+            </Card.Image>
+            <Card.Footer>
+              <h1>
+                Title: {product.title}
+              </h1>
+            </Card.Footer>
+            <Card.Footer>
+              <h1>
+                Description: {product.description !== '' ? product.description : "N/A"}
+              </h1>
+            </Card.Footer>
+            <Card.Footer>
+              <h1>
+                $ {product.price.toFixed(2)}
+              </h1>
+            </Card.Footer>
+            <Card.Footer>
+              <Button.Group>
+                <Button>
+                  S
+                </Button>
+                <Button>
+                  M
+                </Button>
+                <Button>
+                  L
+                </Button>
+                <Button>
+                  XL
+                </Button>
+              </Button.Group>
+            </Card.Footer>
+          </Card>
+        </Column>
+      )}
+    </Column.Group>
+
+
   );
 };
 
